8c0d625a5d8e469689ba70beda42c55a42962447
Anik-hasan07 <iamakashcse@gmail.com>
2024-02-01 03:21:59 +0600

    auth system nextauth

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ["pixner.net", "res.cloudinary.com","encrypted-tbn2.gstatic.com","www.apple.com","adminapi.applegadgetsbd.com"],
+      },
+}
 
 module.exports = nextConfig
